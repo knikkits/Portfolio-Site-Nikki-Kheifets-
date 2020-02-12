ef7892b34cd8cdc51baa93144a505b7c7d914b81
knikkits <nikki_kheifets@hotmail.com>
2020-02-11 21:26:15 -0800

    update .js
    adding jQuery

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log("Hi! Welcome to my portfolio!")
+function menuToggle() {
+   var x = document.getElementById('myNavtoggle');
+   x.className += ' responsive';
+ } else {
+  x.className = 'navtoggle';
+}
