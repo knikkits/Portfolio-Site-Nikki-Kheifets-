1992691251fa0cc6cb256793428020d145942e54
knikkits <nikki_kheifets@hotmail.com>
2020-02-13 01:29:26 -0800

    Update script.js
    butify

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 console.log("Hi! Welcome to my portfolio!")
-function menuToggle () {
+
+function menuToggle() {
   var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
     x.className += ' responsive'
   } else {
     x.className = 'navtoggle'
-  }
+   }
 }
